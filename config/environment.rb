9a7050b2daf2f76b50ae95d5597862f22dd4ed0b
Jared Pace <jared@codewordstudios.com>
2009-08-18 09:47:30 -0400

    Use Authlogic instead of Clearance due to some possible 3.0.pre compatibility issues.

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,6 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
 
-# Clearance E-mails
-DO_NOT_REPLY = 'no-reply@hubbub.com'
-
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
@@ -20,6 +17,7 @@ Rails::Initializer.run do |config|
   # Specify gems that this application depends on and have them installed with rake gems:install
   config.gem "mislav-will_paginate", :lib => "will_paginate", :version => ">= 2.3.2", :source => "http://gems.github.com"
   config.gem "haml", :version => ">= 2.2.1"
+  config.gem "binarylogic-authlogic", :lib => 'authlogic', :source => 'http://gems.github.com'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,5 @@
 # Settings specified here will take precedence over those in config/environment.rb
 
-# Clearance Mailers
-HOST = 'hubbub.com'
-
 # The production environment is meant for finished, "live" apps.
 # Code is not reloaded between requests
 config.cache_classes = true
