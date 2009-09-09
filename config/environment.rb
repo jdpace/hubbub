840242a34bee0fa7a8e66b97219b4dd50ce4bfa6
jdpace <jared@codewordstudios.com>
2009-09-08 22:18:51 -0400

    Clean up gem requirements

--- config/environment.rb
+++ config/environment.rb
@@ -15,9 +15,9 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
-  config.gem "rsl-stringex", :version => ">= 0.9.2", :lib => "stringex", :source => "http://gems.github.com"
-  config.gem "mislav-will_paginate", :lib => "will_paginate", :version => ">= 2.3.2", :source => "http://gems.github.com"
-  config.gem "haml", :version => ">= 2.2.1"
+  config.gem "rsl-stringex", :lib => "stringex", :source => "http://gems.github.com"
+  config.gem "mislav-will_paginate", :lib => "will_paginate", :source => "http://gems.github.com"
+  config.gem "haml"
   config.gem "binarylogic-authlogic", :lib => 'authlogic', :source => 'http://gems.github.com'
   
   # Test related gems
