1ccb1a5be774038b87451854eb1d59631a905b8d
jdpace <jared@codewordstudios.com>
2009-10-19 21:14:53 -0400

    Add a simple post controller for the front end

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,11 @@
 class PostsController < ApplicationController
+  
+  def index
+    @posts = Post.paginate(:page => params[:page], :per_page => App.per_page[:posts])
+  end
+  
+  def show
+    @post = Post.find(params[:id])
+  end
+  
 end
--- test/functional/posts_controller_test.rb
+++ test/functional/posts_controller_test.rb
@@ -1,8 +1,27 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  context "GET /posts" do
+    setup do
+      @posts = []
+      3.times {|t| @posts << Factory.build(:post, :url => "post-#{t}")}
+      Post.stubs(:paginate).returns(@posts)
+      get :index
+    end
+    
+    should_assign_to(:posts) { @posts }
   end
+  
+  
+  context "Get /posts/:id" do
+    setup do
+      @post = Factory.build(:post, :url => 'post', :created_at => 1.day.ago)
+      Post.stubs(:find).with(@post.to_param).returns(@post)
+      get :show, :id => @post.to_param
+    end
+    
+    should_assign_to(:post) { @post }
+  end
+  
 end
