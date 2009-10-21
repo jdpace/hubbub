7d677c73868d965add3dc812a6f6f2d7c09ee2dd
jdpace <jared@codewordstudios.com>
2009-10-21 16:05:49 -0400

    Give the post show page some style

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,8 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
   
+  before_save :format_author_website
+  
   validates_presence_of :post, :body
   validates_presence_of :author_name, :author_email, :unless => :user
   validates_format_of   :author_email, :if => :author_email, :with => Authlogic::Regex.email,
@@ -17,5 +19,20 @@ class Comment < ActiveRecord::Base
     return user.email if user
     self[:author_email]
   end
+  
+  protected
+  
+    # make sure no one leaves site as just http:// and that is starts with that
+    def format_author_website
+      return if author_website.blank?
+      self.author_website = case author_website
+                  when /^http:\/\/$/
+                    nil
+                  when /^http:\/\//
+                    author_website
+                  else
+                    'http://' + author_website
+                  end
+    end
   
 end
