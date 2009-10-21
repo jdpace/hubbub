734931de889f44700ebfa49c8c73ee57b4963646
jdpace <jared@codewordstudios.com>
2009-10-21 09:56:40 -0400

    Use authlogic's email regex since its there already

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,15 +1,11 @@
 class Comment < ActiveRecord::Base
-  EmailNameRegex = '[A-Z0-9_\.%\+\-]+'
-  DomainHeadRegex = '(?:[A-Z0-9\-]+\.)+'
-  DomainTldRegex = '(?:[A-Z]{2,4}|museum|travel)'
-  EmailRegex = /\A#{EmailNameRegex}@#{DomainHeadRegex}#{DomainTldRegex}\z/i
   
   belongs_to :post
   belongs_to :user
   
   validates_presence_of :post, :body
   validates_presence_of :author_name, :author_email, :unless => :user
-  validates_format_of   :author_email, :if => :author_email, :with => EmailRegex,
+  validates_format_of   :author_email, :if => :author_email, :with => Authlogic::Regex.email,
     :message => 'must be a valid email address'
   
 end
