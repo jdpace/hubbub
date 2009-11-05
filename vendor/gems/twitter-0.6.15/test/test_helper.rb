f035759b0f2f50e5927562916d158c016b499972
jdpace <jared@codewordstudios.com>
2009-11-04 19:39:32 -0500

    Vendor gems to make deploys simpler

---
+++ vendor/gems/twitter-0.6.15/test/test_helper.rb
@@ -0,0 +1,42 @@
+require 'test/unit'
+require 'pathname'
+require 'rubygems'
+
+gem 'thoughtbot-shoulda', '>= 2.10.1'
+gem 'jnunemaker-matchy', '0.4.0'
+gem 'mocha', '0.9.4'
+gem 'fakeweb', '>= 1.2.5'
+
+require 'shoulda'
+require 'matchy'
+require 'mocha'
+require 'fakeweb'
+
+FakeWeb.allow_net_connect = false
+
+dir = (Pathname(__FILE__).dirname + '../lib').expand_path
+require dir + 'twitter'
+
+class Test::Unit::TestCase
+end
+
+def fixture_file(filename)
+  return '' if filename == ''
+  file_path = File.expand_path(File.dirname(__FILE__) + '/fixtures/' + filename)
+  File.read(file_path)
+end
+
+def twitter_url(url)
+  url =~ /^http/ ? url : "http://twitter.com:80#{url}"
+end
+
+def stub_get(url, filename, status=nil)
+  options = {:body => fixture_file(filename)}
+  options.merge!({:status => status}) unless status.nil?
+  
+  FakeWeb.register_uri(:get, twitter_url(url), options)
+end
+
+def stub_post(url, filename)
+  FakeWeb.register_uri(:post, twitter_url(url), :body => fixture_file(filename))
+end
